0f1e62d548233bb4fffb9be79fecd4ff9a9aa56f
Rodrigo Sancho <>
2023-09-10 19:09:55 +0200

    Added some extra info to resutls images

--- src/reporting/generate_graphs.py
+++ src/reporting/generate_graphs.py
@@ -1,4 +1,4 @@
-from utils.graphs import show_outliers_map, show_elbow_plot
+from utils.graphs import show_outliers_map, show_flights_map, show_elbow_plot
 from sklearn.base import BaseEstimator, TransformerMixin
 from src.features.epsilon_calculator import EpsilonCalculator
 
@@ -13,6 +13,8 @@ class GenerateGraphs(BaseEstimator, TransformerMixin):
         data = X['data']
         models = X['models']
         trajectories = data['trajectories']
+
+        show_flights_map(trajectories, f'{self.report_path}/flights.png') 
         
         dbscan__sspd__silhouette, dbscan__sspd__davies_bouldin, dbscan__erp__silhouette, dbscan__erp__davies_bouldin, hdbscan__sspd__silhouette, hdbscan__sspd__davies_bouldin, hdbscan__erp__silhouette, hdbscan__erp__davies_bouldin = models
 
@@ -23,8 +25,9 @@ class GenerateGraphs(BaseEstimator, TransformerMixin):
             distance_fn_ = model['distance_fn_']
             labels = model['labels_']
             graphs_path = f'{self.report_path}/{algorithm_}/{distance_fn_}/{validation_method_}'
-            
-            show_outliers_map(trajectories, labels, f'{graphs_path}/outliers.png') 
+            outliers = model['search_']['best_estimator_']['outliers']
+
+            show_outliers_map(trajectories, labels, f'{graphs_path}/outliers.png', outliers) 
 
             if(algorithm_ == 'dbscan'):
               calculator = EpsilonCalculator()
--- utils/graphs.py
+++ utils/graphs.py
@@ -9,6 +9,18 @@ from shapely.geometry import LineString
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 
+
+CODES = {
+  (40.47222, -3.56083): 'MAD', # Madrid
+  (43.30111, -2.91056): 'BIO', # Bilbao
+  (41.29695, 2.07833): 'BCN', # Barcelona
+  (43.30195, -8.37722): 'LCG', # A Coruña
+  (36.74472, -6.06): 'XRY', # Jerez
+  (43.56361, -6.03472): 'OVD', # Asturias
+  (37.18861, -3.77722): 'GRX' # Granada
+}
+
+
 def ensure_saving_path(path):
   directory, file_name = os.path.split(path)
 
@@ -47,7 +59,13 @@ def altitude_profile(trajectories, ax):
   ax.set(xticklabels=[])
   ax.set_ylabel('Flight Level')
 
-def show_outliers_map(trajectories, labels, save_as):
+def pad_number(number):
+  length = 6
+  fill_char = ' '
+  return str(number).rjust(length - len(str(number)), fill_char)   
+
+def show_outliers_map(trajectories, labels, save_as, outliers):
+  total = len(trajectories)
   ax = plt.axes(projection=ccrs.PlateCarree())
   ax.patch.set_facecolor('white')
 
@@ -59,6 +77,17 @@ def show_outliers_map(trajectories, labels, save_as):
   ax.add_feature(cfeature.BORDERS.with_scale('50m'), linewidth=0.5)
   ax.set_extent([-10, 5, 35, 45], crs=ccrs.PlateCarree())
 
+  annotated = {}
+  for trajectory in trajectories:
+      if tuple(trajectory[0]) not in annotated:
+          coords = (trajectory[0][1], trajectory[0][0])
+          ax.annotate(CODES[tuple(trajectory[0])], xy=coords, xytext=(5,-5), textcoords='offset points', fontsize=5, bbox=dict(facecolor='white', alpha=0.5))
+          annotated[tuple(trajectory[0])] = True
+      if tuple(trajectory[-1]) not in annotated:
+          coords = (trajectory[-1][1], trajectory[-1][0])
+          ax.annotate(CODES[tuple(trajectory[-1])], xy=coords, xytext=(5,-5), textcoords='offset points', fontsize=5, bbox=dict(facecolor='white', alpha=0.5))
+          annotated[tuple(trajectory[-1])] = True
+
   geometry = [LineString([(point[1], point[0]) for point in trajectory]) for trajectory in trajectories]
   data = {'label': labels, 'geometry': geometry}
   routes = gpd.GeoDataFrame(data, crs='EPSG:4326')
@@ -68,6 +97,47 @@ def show_outliers_map(trajectories, labels, save_as):
   
   routes['alpha'] = 0.3
   routes.loc[labels == -1, 'alpha'] = 1
+
+  routes.plot(ax=ax, transform=ccrs.Geodetic(), color=routes['color'], linewidth=0.2, alpha=routes['alpha'])
+  
+  ensure_saving_path(save_as)
+
+  plt.annotate('Vuelos totales: {}\nVuelos anómalos: {}'.format(pad_number(total), pad_number(outliers)), 
+               xy=(1.0, 0.0), xycoords='axes fraction', fontsize=10, 
+               xytext=(-5, 5), textcoords='offset points', ha='right', va='bottom', 
+               bbox=dict(boxstyle='square', fc='white', ec='white'))
+  plt.savefig(save_as, dpi=600, bbox_inches='tight')
+  plt.close()
+
+def show_flights_map(trajectories, save_as):
+  ax = plt.axes(projection=ccrs.PlateCarree())
+  ax.patch.set_facecolor('white')
+
+  plt.setp(ax.spines.values(), color='white')
+  plt.setp([ax.get_xticklines(), ax.get_yticklines()], color='white')
+
+  ax.add_feature(cfeature.LAND.with_scale('50m'), facecolor=cfeature.COLORS['land'])
+  ax.add_feature(cfeature.OCEAN.with_scale('50m'), facecolor=cfeature.COLORS['water'])
+  ax.add_feature(cfeature.BORDERS.with_scale('50m'), linewidth=0.5)
+  ax.set_extent([-10, 5, 35, 45], crs=ccrs.PlateCarree())
+
+  annotated = {}
+  for trajectory in trajectories:
+      if tuple(trajectory[0]) not in annotated:
+          coords = (trajectory[0][1], trajectory[0][0])
+          ax.annotate(CODES[tuple(trajectory[0])], xy=coords, xytext=(5,-5), textcoords='offset points', fontsize=6, bbox=dict(facecolor='white', alpha=0.5))
+          annotated[tuple(trajectory[0])] = True
+      if tuple(trajectory[-1]) not in annotated:
+          coords = (trajectory[-1][1], trajectory[-1][0])
+          ax.annotate(CODES[tuple(trajectory[-1])], xy=coords, xytext=(5,-5), textcoords='offset points', fontsize=6, bbox=dict(facecolor='white', alpha=0.5))
+          annotated[tuple(trajectory[-1])] = True
+
+  geometry = [LineString([(point[1], point[0]) for point in trajectory]) for trajectory in trajectories]
+  data = {'geometry': geometry}
+  routes = gpd.GeoDataFrame(data, crs='EPSG:4326')
+
+  routes['color'] = 'blue'
+  routes['alpha'] = 0.3
 
   routes.plot(ax=ax, transform=ccrs.Geodetic(), color=routes['color'], linewidth=0.2, alpha=routes['alpha'])
   
